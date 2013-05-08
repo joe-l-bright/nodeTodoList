ea4df5ce96b26fb5598d1593180c93e6606a57c7
joe-l-bright <keljoebright@gmail.com>
2013-05-07 23:03:38 -0500

    commit

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+    , routes = require('./routes')
+    , user = require('./routes/user')
+    , http = require('http')
+    , db = require('./db/mongo')
+    , path = require('path');
+
 
 var app = express();
 
@@ -19,17 +20,27 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.cookieParser());
+app.use(routes.current_user);
+app.use(app.router);
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function () {
+    app.use(express.errorHandler());
+});
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.post('/create', routes.create);
+app.get('/destroy/:id', routes.destroy);
+app.get('/edit/:id', routes.edit);
+app.post('/update/:id', routes.update);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,57 @@
+var mongoose = require('mongoose');
+var Todo = mongoose.model('Todo');
 
-/*
- * GET home page.
- */
+exports.create = function (req, res) {
+    new Todo({
+        content: req.body.content,
+        updated_at: Date.now()
+    }).save(function (err, todo, count) {
+            res.redirect('/');
+        });
+};
+exports.destroy = function (req, res) {
+    Todo.findById(req.params.id, function (err, todo) {
+        todo.remove(function (err, todo) {
+            res.redirect('/');
+        });
+    });
+};
+exports.index = function (req, res) {
+    Todo.
+        find().
+        sort('-updated_at').
+        exec(function (err, todos) {
+            res.render('index', {
+                title: 'Express Todo Example',
+                todos: todos
+            });
+        });
+};
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.edit = function (req, res) {
+    Todo.
+        find().
+        sort('-updated_at').
+        exec(function (err, todos) {
+            res.render('edit', {
+                title: 'Express Todo Example',
+                todos: todos,
+                current: req.params.id
+            });
+        });
+};
+exports.update = function (req, res) {
+    Todo.findById(req.params.id, function (err, todo) {
+        todo.content = req.body.content;
+        todo.updated_at = Date.now();
+        todo.save(function (err, todo, count) {
+            res.redirect('/');
+        });
+    });
+};
+exports.current_user = function (req, res, next) {
+    if (!req.cookies.user_id) {
+        res.cookie('user_id', utils.uid(32));
+    }
+    next();
 };
