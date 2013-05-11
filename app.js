3d82888c264ebd0f114d04885d9acca2949352b0
joe-l-bright <keljoebright@gmail.com>
2013-05-11 13:24:30 -0500

    commit

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var express = require('express')
     , routes = require('./routes')
     , user = require('./routes/user')
     , http = require('http')
-    , db = require('./db/mongo')
     , path = require('path');
 
 
@@ -36,10 +35,11 @@ app.configure('production', function () {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.post('/create', routes.create);
 app.get('/destroy/:id', routes.destroy);
 app.get('/edit/:id', routes.edit);
+
 app.post('/update/:id', routes.update);
+app.post('/create', routes.create);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
+    , db = require('./db')
 var Todo = mongoose.model('Todo');
 
 exports.create = function (req, res) {
